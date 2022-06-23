7a3535658603fbb08d5af6d5aa3e8b8a61e5a18c
zeyno12 <zeynabbagirova1@gmail.com>
2022-06-23 10:29:26 +0400

    ugursuz emeliyyat

---
+++ script.js
@@ -0,0 +1,21 @@
+arr = [];  
+const empty = arr => arr.length = 0;
+var arr= [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+empty(arr);
+function salam(){
+let number;
+number= Number(document.getElementById("name").value)
+} 
+const numberbtn=document.querySelector(".btn");
+if(numberbtn){
+    numberbtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        if(number<=1234567812345678){
+            alert("true")
+        }
+        else{
+            alert("false")
+        }
+        
+    })
+}
